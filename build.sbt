218bc499def1097a314a658cf2f39655b71dac95
isshivam <shivam.rai@knoldus.com>
2021-08-19 13:54:57 +0530

    initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "OOPS_ASIGNMENT_2"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
---
+++ src/main/scala/Demo.scala
@@ -0,0 +1,16 @@
+object demo {
+
+  def main(args: Array[String]) {
+
+    var personOne = new Person("test", 21)
+    var personTwo = new Person("testUser", 21)
+
+    personOne.check(personTwo)
+
+
+
+  }
+
+
+}
+
